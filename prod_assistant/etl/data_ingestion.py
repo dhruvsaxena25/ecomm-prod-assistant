2705e6929905dfe7ca8ae0212101f358237939fa
Dhruv-saxena-25 <saxena25dhruv@gmail.com>
2025-08-31 15:52:49 +0530

    data ingestion for etl pipeline

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -3,22 +3,35 @@ class DataIngestion:
     Class to handle data transformation and ingestion into AstraDB vector store.
     """
     def __init__(self):
+        """Initialize the DataIngestion class."""
         pass
     
     def _load_env_variables(self):
+        """Load environment variables from a .env file.
+        """
         pass
         
     def _get_csv_path(self):
+        """Get the path to the CSV file.
+        """
         pass
     
     def _load_csv(self):
+        """Load data from the CSV file.
+        """
         pass
     
     def transform_data(self):
+        """Transform the loaded data into the desired format.
+        """
         pass
     
     def store_in_vector_db(self):
+        """Store the transformed data into the AstraDB vector store.
+        """
         pass
     
     def run_pipeline(self):
+        """Run the data ingestion pipeline.
+        """
         pass
