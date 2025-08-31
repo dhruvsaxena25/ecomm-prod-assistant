d0cc1cadf58a3a6b246313a0e51d5bdd45552f5f
Dhruv-saxena-25 <saxena25dhruv@gmail.com>
2025-08-31 15:51:40 +0530

    data ingestion for etl

---
+++ prod_assistant/etl/data_ingestion.py
@@ -0,0 +1,24 @@
+class DataIngestion:
+    """
+    Class to handle data transformation and ingestion into AstraDB vector store.
+    """
+    def __init__(self):
+        pass
+    
+    def _load_env_variables(self):
+        pass
+        
+    def _get_csv_path(self):
+        pass
+    
+    def _load_csv(self):
+        pass
+    
+    def transform_data(self):
+        pass
+    
+    def store_in_vector_db(self):
+        pass
+    
+    def run_pipeline(self):
+        pass
